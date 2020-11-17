fbc25e67fbd892436255ef63a82f450350042e28
luisjavierjn <luisjavierjn@gmail.com>
2020-11-17 15:23:54 -0500

    communication between front and back works

--- src/main/webapp/src/app/app.module.ts
+++ src/main/webapp/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InitialSetupComponent } from './initial-setup/initial-setup.component';
 import { AppRouting } from "./app.routing";
+import {ReactiveFormsModule} from "@angular/forms";
+import { SearchService } from './services/search.service';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
@@ -13,8 +16,10 @@ import { AppRouting } from "./app.routing";
   imports: [
     BrowserModule,
     AppRouting,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [SearchService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/main/webapp/src/app/initial-setup/initial-setup.component.ts
+++ src/main/webapp/src/app/initial-setup/initial-setup.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {SearchService} from "../services/search.service";
 
 @Component({
   selector: 'app-initial-setup',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InitialSetupComponent implements OnInit {
 
-  constructor() { }
+  initialSetupForm: FormGroup;
+  invalidInitialSetup: boolean = false;
+  constructor(private formBuilder: FormBuilder, private router: Router, private searchService: SearchService) { }
+
+  onSubmit() {
+    if (this.initialSetupForm.invalid) {
+      return;
+    }
+    const payload = {
+      offset: this.initialSetupForm.controls.offset.value,
+      size: this.initialSetupForm.controls.size.value,
+      aggregate: false
+    }
+    this.searchService.getCompaniesByInitialSetup(payload).subscribe(data => {
+      if(data.status === 200) {
+        alert(data.result);
+        console.log(data.result);
+        this.router.navigate(['analyzer']);
+      }else {
+        this.invalidInitialSetup = true;
+        alert(data.message);
+      }
+    });
+  }
 
   ngOnInit(): void {
+    this.initialSetupForm = this.formBuilder.group({
+      offset: ['0', Validators.compose([Validators.required])],
+      size: ['100', Validators.required]
+    });
   }
 
 }
